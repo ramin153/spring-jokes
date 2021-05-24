048bed709ed321d52df2d5bf9a1441cca1136e63
ramin rowshan <raminrowshan153@gmail.com>
2021-05-24 23:10:28 +0430

    init

---
+++ src/test/java/com/example/springjokes/SpringJokesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springjokes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJokesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
